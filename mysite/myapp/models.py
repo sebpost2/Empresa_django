86ba68cdd3817de01e4805e1a061b9eed2c87fcf
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2023-11-06 08:31:56 -0500

    Delete mysite directory

--- mysite/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class pregunta(models.Model):
-    Fecha = models.DateTimeField(auto_now_add=True)
-    Descripcion = models.CharField(max_length=1000)
-    Carrera = models.CharField(max_length=25)
-    Votos=models.IntegerField()
